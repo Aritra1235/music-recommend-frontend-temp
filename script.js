f011770e1253d5a9e1402a2ac7889fa5a2a940d5
Aritra Bhattacharya <aritra1235@gmail.com>
2023-10-10 02:04:40 +0530

    added links

--- script.js
+++ script.js
@@ -31,6 +31,18 @@ document.addEventListener('DOMContentLoaded', function () {
                 document.getElementById('song8').textContent = "Recommend 8: " + data.songs[7].song8 + " by " + data.artists[7].artist8;
                 document.getElementById('song9').textContent = "Recommend 9: " + data.songs[8].song9 + " by " + data.artists[8].artist9;
                 document.getElementById('song10').textContent = "Recommend 10: " + data.songs[9].song10 + " by " + data.artists[9].artist10;
+                document.getElementById('songlink1').href = "https://open.spotify.com/track/" + data.ids[0].id1;
+                document.getElementById('songlink2').href = "https://open.spotify.com/track/" + data.ids[1].id2;
+                document.getElementById('songlink3').href = "https://open.spotify.com/track/" + data.ids[2].id3;
+                document.getElementById('songlink4').href = "https://open.spotify.com/track/" + data.ids[3].id4;
+                document.getElementById('songlink5').href = "https://open.spotify.com/track/" + data.ids[4].id5;
+                document.getElementById('songlink6').href = "https://open.spotify.com/track/" + data.ids[5].id6;
+                document.getElementById('songlink7').href = "https://open.spotify.com/track/" + data.ids[6].id7;
+                document.getElementById('songlink8').href = "https://open.spotify.com/track/" + data.ids[7].id8;
+                document.getElementById('songlink9').href = "https://open.spotify.com/track/" + data.ids[8].id9;
+                document.getElementById('songlink10').href = "https://open.spotify.com/track/" + data.ids[9].id10;
+                
+                
                 var songData = document.getElementById('songData');
                 songData.classList.remove('hidden');
 
